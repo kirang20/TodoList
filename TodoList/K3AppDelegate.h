15f0d50614b9acc25858a04c6f5b351edf9cd7e2
Kiran <kirang20@gmail.com>
2014-05-14 13:33:08 +0530

    Initial Commit

---
+++ TodoList/K3AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  K3AppDelegate.h
+//  TodoList
+//
+//  Created by Kiran on 14/05/14.
+//  Copyright (c) 2014 Kiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface K3AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoList
+//
+//  Created by Kiran on 14/05/14.
+//  Copyright (c) 2014 Kiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "K3AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([K3AppDelegate class]));
+    }
+}
